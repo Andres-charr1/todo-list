c62eecfcd0fd2f21109c2b1f2bf4daed4d47c4ba
Andres-charr1 <andres.charry04@hotmail.com>
2025-03-17 23:52:03 -0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Todo_list_p',
+  webDir: 'www'
+};
+
+export default config;
